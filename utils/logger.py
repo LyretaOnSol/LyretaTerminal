6a6a8985aef2edb0a5664fb6ff340e7672c2330c
LyretaOnSol <lakgskghsd@gmail.com>
2024-12-02 17:32:16 +0100

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -3,4 +3,4 @@ import logging
 def setup_logger():
     """Configure and return a logger instance."""
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-    return logging.getLogger("AeuraAgent")
+    return logging.getLogger("LyretaAgent")
